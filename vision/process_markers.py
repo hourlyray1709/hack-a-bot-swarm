31bf199df040cf2a5d3100bdcf172e1ef874258d
Wai Hei CHAN <wai.chan-12@student.manchester.ac.uk>
2026-03-22 04:38:30 +0000

    test drive

--- vision/process_markers.py
+++ vision/process_markers.py
@@ -89,7 +89,7 @@ def get_data(corner_data, model):
         cv2.imshow("preview", frame)
         corner_data.ids = ids
         rval, rawframe = vc.read()
-        frame = cv2.undistort(rawframe, mtx, dist, None)
+        #frame = cv2.undistort(rawframe, mtx, dist, None)
         #frame = cv2.absdiff(frame, background)
         key = cv2.waitKey(20)
         if key == 27: # exit on ESC
